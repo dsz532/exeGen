ae7c25bb69b2cc52aa9ce62cdc935cd8cbba40b3
dszfzzzm <dszfzzm@163.com>
2024-12-10 15:48:13 +0800

    1

--- process.py
+++ process.py
@@ -2,28 +2,82 @@ from agents import *
 
 agent_kt.description = "a knowledge tracking expert"
 agent_exeGen_generator.description = "an exercise generation expert"
-agent_exeGen_discriminator.description = "an exercise evaluation expert"
-agent_host.description = "moderator of this meeting"
+agent_exeGen_discriminator_1.description = "exercise evaluation expert 1"
+agent_exeGen_discriminator_2.description = "exercise evaluation expert 2"
+agent_exeGen_discriminator_3.description = "exercise evaluation expert 3"
+agent_exeGen_discriminator_4.description = "exercise evaluation expert 4"
+agent_exeGen_discriminator_5.description = "exercise evaluation expert 5"
+agent_host.description = "the host of this meeting"
 
-groupchat = GroupChat(
-    agents=[agent_kt, agent_exeGen_generator, agent_exeGen_discriminator, agent_host],
+out_groupchat = GroupChat(
+    agents=[agent_kt, agent_exeGen_generator, agent_exeGen_discriminator_1, agent_exeGen_discriminator_2,
+            agent_exeGen_discriminator_3, agent_exeGen_discriminator_4, agent_exeGen_discriminator_5, agent_host],
     messages=[],
-    max_round=6,
     send_introductions=True,
 )
 
-groupchat_manager = GroupChatManager(
-    groupchat=groupchat,
+out_groupchat_manager = GroupChatManager(
+    groupchat=out_groupchat,
     llm_config=llm_config,
 )
 
 file = open("test.txt", "r")
 text = file.read()
+file.close()
+
+# chat_res = agent_host.initiate_chats(
+#     [
+#         {
+#             "recipient": agent_kt,
+#             "message": text,
+#             "max_turns": 2,
+#             "summary_method": "last_msg",
+#         },
+#         {
+#             "recipient": agent_exeGen_generator,
+#             "message": "",
+#             "max_turns": 2,
+#             "summary_method": "last_msg",
+#         },
+#         {
+#             "recipient": agent_exeGen_discriminator_1,
+#             "message": "",
+#             "max_turns": 2,
+#             "summary_method": "last_msg",
+#         },
+#         {
+#             "recipient": agent_exeGen_discriminator_2,
+#             "message": "",
+#             "max_turns": 2,
+#             "summary_method": "last_msg",
+#         },
+#         {
+#             "recipient": agent_exeGen_discriminator_3,
+#             "message": "",
+#             "max_turns": 2,
+#             "summary_method": "last_msg",
+#         },
+#         {
+#             "recipient": agent_exeGen_discriminator_4,
+#             "message": "",
+#             "max_turns": 2,
+#             "summary_method": "last_msg",
+#         },
+#         {
+#             "recipient": agent_exeGen_discriminator_5,
+#             "message": "",
+#             "max_turns": 2,
+#             "summary_method": "last_msg",
+#         },
+#     ]
+# )
 
 chat_res = agent_host.initiate_chat(
-    groupchat_manager,
-    message=text,
+    out_groupchat_manager,
+    message="You are the moderator of this meeting; "
+            "first, you will obtain a list containing question information and students' answer statuses. You need to pass this list to agent_kt to generate a knowledge state. "
+            "Then, you need to have agent_exeGen_generator create new questions. "
+            "Finally, you need to submit the newly generated questions to all agent_exeGen_discriminators for evaluation. "
+            "If they deem the exercises unsatisfactory, you will need to go back to the previous step and have agent_exeGen_generator generate the questions again." + text,
     summary_method="reflection_with_llm",
 )
-
-print(chat_res.summary)
