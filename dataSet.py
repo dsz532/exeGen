178be6105171036d4323fd5871b42a365819aec6
dszfzzzm <dszfzzm@163.com>
2024-12-22 20:05:43 +0800

    1

--- dataSet.py
+++ dataSet.py
@@ -9,12 +9,24 @@ user_problem = pandas.read_csv("subject_data(1)/user_problem.csv")
 stuRec = pandas.read_csv("subject_data(1)/stuRec.csv")
 example = pandas.read_csv("subject_data(1)/example.csv")
 examples_with_explanation = pandas.read_csv("subject_data(1)/examples_with_explanation.csv")
+concept_relation_filtered = pandas.read_csv("subject_data(1)/concept_relationship_filtered.csv")
+course_profile = pandas.read_csv("subject_data(1)/course_profile.csv")
+course_problem = pandas.read_csv("subject_data(1)/course_problem.csv")
+
 
 # 构造example
 # top3_problems = problem_concept.groupby('concept_id').head(1).reset_index(drop=True)
 # result_df = pandas.merge(problem_concept, problem, on='problem_id', how='left')
 # result_df = pandas.merge(user_problem, result_df, on='problem_id', how='left')
 # result_df = result_df.dropna(subset=['title'])
+# result_df.to_csv("subject_data(1)/example.csv", index=False)
+
+# 添加课程信息
+# stuRec = pandas.merge(stuRec, course_problem, on="problem_id", how="left")
+# selected_columns = ["course_id", "name"]
+# course_profile = course_profile[selected_columns]
+# stuRec = pandas.merge(stuRec, course_profile, on="course_id", how="left")
+# stuRec.to_csv("subject_data(1)/stuRec.csv", index=False)
 
 # 获取一个用户的作答记录
 # result_df = pandas.merge(problem_concept, problem, on='problem_id', how='left')
@@ -29,45 +41,59 @@ examples_with_explanation = pandas.read_csv("subject_data(1)/examples_with_expla
 
 # 添加带有explanation属性的example表
 # example["explanation"] = "No explanation provided."
-# selected_columns = ['content', 'option', 'answer', 'is_correct', 'explanation']
+# selected_columns = ['content', 'option', 'answer', 'is_correct', 'concept_id','course_name', 'knowledge_chain', 'explanation']
 # df_selected = example[selected_columns]
-# df_selected = df_selected.head(20)
 # df_selected.to_csv('subject_data(1)/examples_with_explanation.csv', index=False)
+
+# 删除所有无效行
+# df_filtered = concept_relation[concept_relation['ground_truth'] != 0]  # 删除值为0的行
+# df_filtered = df_filtered[df_filtered['ground_truth'] != -1]  # 删除值为-1的行
+#
+# df_filtered.to_csv('subject_data(1)/concept_relationship_filtered.csv', index=False)
 
 # 递归计算知识链
 # example["knowledge_chain"] = ""
-#
-#
-# def get_chain(concept, chain):
-#     filtered_df = concept_relation[concept_relation['c1'] == concept]
-#     if not filtered_df.empty:
-#         # 若还存在上级知识点，则链接新链后进行递归
-#         first_row = filtered_df.head(1)
-#         upper_concept = first_row['c2'].iloc[0]
-#         chain = upper_concept + "-" + chain
-#         return get_chain(upper_concept, chain)
-#     else:
-#         return chain
-#
-#
+
+
+def get_chain(concept, chain):
+    filtered_df = concept_relation_filtered[concept_relation_filtered['c2'] == concept]
+    if not filtered_df.empty:
+        # 若还存在上级知识点，则链接新链后进行递归
+        first_row = filtered_df.head(1)
+        upper_concept = first_row['c1'].iloc[0]
+        chain = upper_concept + "-" + chain
+        return get_chain(upper_concept, chain)
+    else:
+        return chain
+
+
 # for index, row in example.iterrows():
 #     chain = get_chain(row['concept_id'], row['concept_id'])
 #     example.at[index, 'knowledge_chain'] = chain
 #
 # example.to_csv("subject_data(1)/example.csv", index=False)
 
+# 构造提示词
 text = {
     "examples": [],
     "tasks": []
 }
 
-# 构造提示词
+examples_with_explanation = examples_with_explanation.groupby('concept_id').head(1)
+examples_with_explanation = pandas.merge(stuRec[['concept_id']].head(10), examples_with_explanation, on='concept_id',
+                                         how='left')
 example_data = examples_with_explanation.to_dict(orient='records')
 text["examples"] = example_data
 
-selected_columns = ['content', 'option', 'answer', 'concept_id', 'is_correct']
+selected_columns = ['content', 'option', 'answer', 'course_name', 'concept_id', 'is_correct']
 stuRec = stuRec[selected_columns]
-stuRec = stuRec.head(20)
+
+stuRec["knowledge_chain"] = ""
+for index, row in stuRec.iterrows():
+    chain = get_chain(row['concept_id'], row['concept_id'])
+    stuRec.at[index, 'knowledge_chain'] = chain
+
+stuRec = stuRec.head(10)
 stuRec["explanation"] = ""
 stuRec = stuRec.to_dict(orient='records')
 text["tasks"] = stuRec
@@ -76,8 +102,8 @@ text = json.dumps(text, ensure_ascii=False, indent=4)
 
 # 排查循环错误
 # a = 0
-# for index1, row1 in concept_relation.iterrows():
-#     for index2, row2 in concept_relation.iterrows():
+# for index1, row1 in concept_relation_filtered.iterrows():
+#     for index2, row2 in concept_relation_filtered.iterrows():
 #         if row1['c1'] == row2['c2'] and row1['c2'] == row2['c1']:
 #             print(row1['c1'], row1['c2'])
 #             a = a + 1
